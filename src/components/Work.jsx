4e8017bd32b5dc81404bc2ac7173a364d71437c8
shalu1529 <144725677+shalu1529@users.noreply.github.com>
2024-04-04 23:01:45 +0530

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,7 +22,7 @@ const Work = () => {
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font=bold inline border-b-4 text-gray-300 border-[#DEB992]'>Project</p>
-                    <p className='py-6'>// Check out some of my work</p>
+                    <p className='py-6'>Check out some of my work</p>
                 </div>
                 
                  
