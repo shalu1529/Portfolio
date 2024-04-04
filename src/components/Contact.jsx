a54707e44de670422fd1a53fd45af69aef2fd310
shalu1529 <144725677+shalu1529@users.noreply.github.com>
2024-04-04 23:02:41 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () =>{
         <form method ='POST'action ="https://getform.io/f/pagkeeza" className ='flex flex-col max-w-[600px] w-full'>
            <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-[#DEB992] text-gray-300'>Contact</p>
-            <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - shaalu1529@gmail.com</p>
+            <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - shaalu1529@gmail.com</p>
             </div>
 
             <input className='bg-[#ccd6f6] p-2' type ="text" placeholder='Name' name ='name' />
