3510b6d8cba2b54ee9956d42dbf379677b076f9c
shalu1529 <144725677+shalu1529@users.noreply.github.com>
2024-04-04 23:01:22 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,7 +8,7 @@ const Project =() =>{
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font=bold inline border-b-4 text-gray-300 border-[#DEB992]'>Project</p>
-                    <p className='py-6'>// Check out some of my work</p>
+                    <p className='py-6'> Check out some of my work</p>
                 </div>
 
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
